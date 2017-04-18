2798362416f67f4ec99616be5575a901af6d6e00
Vitalii Grygoruk <vitaliy.grigoruk@gmail.com>
2017-04-18 10:30:57 +0200

    Revert "production debug"
    This reverts commit 43866163e42c82b7b8fa5471ea17e1f65c999b43.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.web_console.development_only = false
 end
