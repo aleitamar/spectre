38c52535cd43ed419cf8cf4ba8004ca97db1bd01
Vitalii Grygoruk <vitaliy.grigoruk@gmail.com>
2017-05-02 11:28:05 +0200

    fix updating of existing test if same screenshot is submitted twice

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -26,9 +26,18 @@ class TestsController < ApplicationController
   private
 
   def find_or_create_test(test_params)
-    search_params = test_params.reject { |k,_| k.to_sym == :screenshot }
-    test = Test.find_or_create_by(search_params)
-    test.screenshot = test_params[:screenshot]
+    search_params = test_params.slice(:name, :browser, :size, :run_id)
+    if test = Test.find_by(search_params)
+      test.screenshot = test_params[:screenshot]
+      test.source_url = test_params[:source_url]
+      test.fuzz_level = test_params[:fuzz_level]
+      test.highlight_colour = test_params[:highlight_colour]
+      test.crop_area = test_params[:crop_area]
+      test.diff_threshold = test_params[:diff_threshold]
+      test.save!
+    else
+      test = Test.create!(test_params)
+    end
     test
   end
 
